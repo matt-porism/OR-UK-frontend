b38b74e7530466b57fd5952eee520d4e9e833ed4
Endy Ranaudo <endy@wearesnook.com>
2021-03-24 16:48:43 +0000

    implements very basic react app i on AWS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 import './App.css';
 
 function App() {
+
+  const URL = 'http://54.78.155.180:1337/landing-page'
+
+  const [headerText, setHeaderText] = useState("");
+  const [bodyText, setBodyText] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+
+  useEffect(() => {
+    // fetch from strapi
+    fetch(URL).then(res => res.json())
+    .then( data => {
+      console.log('DATA', data)
+      // set data from strapi to state vars
+      setHeaderText(data.title);
+      setBodyText(data.projectOverview)
+      setImageUrl(data.logo.name)
+    })
+  }, []);
+
+
   return (
     <div className="App">
       <Header />
-      <main class="content">…</main>
+      <main class="content">
+        <h2>{headerText} </h2>
+        <p>{bodyText}</p>
+        <img className="logo" src={imageUrl} alt="logo"/>
+      </main>
       <Footer className="footer" />
     </div>
   );
