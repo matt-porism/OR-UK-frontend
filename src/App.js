1c7b546d0d24dedc0a71a4297a4ced61a00e7fe9
Endy Ranaudo <endy@wearesnook.com>
2021-03-31 16:30:27 +0100

    small code formatting changes

--- src/App.js
+++ src/App.js
@@ -1,78 +1,80 @@
 import { useState, useEffect } from 'react';
 import ReactMarkdown from 'react-markdown';
-import '../node_modules/jquery/dist/jquery.min.js'
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
-import SideMenu from './components/sidemenu/SideMenu'
+import '../node_modules/jquery/dist/jquery.min.js';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import SideMenu from './components/sidemenu/SideMenu';
 import './App.css';
 
 function App() {
+  const BASE_URL = 'http://54.78.155.180:1337';
+  const LANDING_PAGE_URI = '/landing-page';
+  const MENU_URI = '/top-menus';
+  const SUB_MENU_URI = '/sub-menus/';
 
-  const BASE_URL = 'http://54.78.155.180:1337'
-  const LANDING_PAGE_URI = '/landing-page'
-  const MENU_URI = '/top-menus'
-  const SUB_MENU_URI = '/sub-menus/'
-
-  const [headerText, setHeaderText] = useState("");
-  const [bodyText, setBodyText] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
-  const [subMenuId, setSubMenuId] = useState("");
-  const [topMenuId, setTopMenuId] = useState("");
+  const [headerText, setHeaderText] = useState('');
+  const [bodyText, setBodyText] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [subMenuId, setSubMenuId] = useState('');
+  const [topMenuId, setTopMenuId] = useState('');
   const [mainMenu, setMainMenu] = useState([]);
-  const [subMenu, setSubMenu] = useState([]); 
+  const [subMenu, setSubMenu] = useState([]);
 
   useEffect(() => {
     // fetch from strapi
-    fetch(BASE_URL + LANDING_PAGE_URI).then(res => res.json())
-    .then( data => {
-      // set data from strapi to state vars
-      setHeaderText(data.title);
-      setBodyText(data.projectOverview);
-	  if (data.sub_menu){
-		setSubMenuId(data.sub_menu.id);
-	  }
-	  if (data.top_menu){
-		setTopMenuId(data.top_menu.id);
-	  } 
-	  if (data.logo){
-		setImageUrl(data.logo.name);
-	  }
-    })
-	
-	fetch(BASE_URL + MENU_URI).then(res => res.json())
-    .then(data => setMainMenu(data))
+    fetch(BASE_URL + LANDING_PAGE_URI)
+      .then((res) => res.json())
+      .then((data) => {
+        // set data from strapi to state vars
+        setHeaderText(data.title);
+        setBodyText(data.projectOverview);
+        if (data.sub_menu) {
+          setSubMenuId(data.sub_menu.id);
+        }
+        if (data.top_menu) {
+          setTopMenuId(data.top_menu.id);
+        }
+        if (data.logo) {
+          setImageUrl(data.logo.name);
+        }
+      });
+
+    fetch(BASE_URL + MENU_URI)
+      .then((res) => res.json())
+      .then((data) => setMainMenu(data));
   }, []);
-  
+
   useEffect(() => {
-	if (subMenuId){		
-		fetch(BASE_URL + SUB_MENU_URI + subMenuId).then(res => res.json())
-		.then(data => setSubMenu(data.MenuItem))
-	}	  
+    if (subMenuId) {
+      fetch(BASE_URL + SUB_MENU_URI + subMenuId)
+        .then((res) => res.json())
+        .then((data) => setSubMenu(data.MenuItem));
+    }
   }, [subMenuId]);
-  
+
   let side;
-  let mainPanelCss = "col-12";
-  
-  if (subMenu && subMenu.length > 0){
-	  mainPanelCss = "col-10";
-	  side = <SideMenu subMenu={subMenu}/>
+  let mainPanelCss = 'col-12';
+
+  if (subMenu && subMenu.length > 0) {
+    mainPanelCss = 'col-10';
+    side = <SideMenu subMenu={subMenu} />;
   }
 
   return (
     <div className="App">
-      <Header mainMenu={mainMenu} topMenuId={topMenuId}/>
-      <div className="container-fluid">        
-		<div className="row">
-		{side}
-		<div className={mainPanelCss}>
-		<main>
-		<h2>{headerText}</h2>
-          <ReactMarkdown>{bodyText}</ReactMarkdown>
-          <img className="logo" src={imageUrl} alt="logo"/>
-        </main>
-			  </div>
-		  </div>
+      <Header mainMenu={mainMenu} topMenuId={topMenuId} />
+      <div className="container-fluid">
+        <div className="row">
+          {side}
+          <div className={mainPanelCss}>
+            <main>
+              <h2>{headerText}</h2>
+              <ReactMarkdown>{bodyText}</ReactMarkdown>
+              <img className="logo" src={imageUrl} alt="logo" />
+            </main>
+          </div>
+        </div>
       </div>
       <Footer className="footer" />
     </div>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,41 +1,52 @@
-import React, { Component } from 'react'
-import './header.scss'
-import Logo from '../../assets/logo.png'
+import React, { Component } from 'react';
+import './header.scss';
+import Logo from '../../assets/logo.png';
 
 export interface HeaderProps {
-    mainMenu: array;
-	topMenuId?: number;
+  mainMenu: array;
+  topMenuId?: number;
 }
 
 export class Header extends Component<HeaderProps> {
   render() {
-	const { mainMenu, topMenuId } = this.props;
+    const { mainMenu, topMenuId } = this.props;
     return (
       <header>
         <div className="container-fluid">
           <div>
-            <img src={Logo} alt="Open Referral Logo"/>
+            <img src={Logo} alt="Open Referral Logo" />
           </div>
         </div>
-		<div id="main_content_wrap" className="outer">
-		<section><div className="navbackground">
-    <nav className="navbar navbar-expand-md navwidth navbar-dark"><ul className="navbar-nav mr-auto">
-		{mainMenu.map(function(name, index){
-			var cssClass = 'nav-item';
-			
-			if (mainMenu[index] != null && mainMenu[index].id === topMenuId)
-			{
-				cssClass += ' active';
-			}
-            return <li className={cssClass} key={mainMenu[index].link}><a className="nav-link" href={mainMenu[index].link}>{mainMenu[index].label}</a></li>;
-        })}
-        </ul>		
-    </nav>
-</div>
-</section></div>
+        <div id="main_content_wrap" className="outer">
+          <section>
+            <div className="navbackground">
+              <nav className="navbar navbar-expand-md navwidth navbar-dark">
+                <ul className="navbar-nav mr-auto">
+                  {mainMenu.map(function (name, index) {
+                    var cssClass = 'nav-item';
+
+                    if (
+                      mainMenu[index] != null &&
+                      mainMenu[index].id === topMenuId
+                    ) {
+                      cssClass += ' active';
+                    }
+                    return (
+                      <li className={cssClass} key={mainMenu[index].link}>
+                        <a className="nav-link" href={mainMenu[index].link}>
+                          {mainMenu[index].label}
+                        </a>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </nav>
+            </div>
+          </section>
+        </div>
       </header>
-    )
+    );
   }
 }
 
-export default Header
+export default Header;
