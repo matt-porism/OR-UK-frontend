f539485ba008fe205d37b328a3f2a05fcec4357a
kingdomlevel <nllmrrs@aol.com>
2021-04-01 09:36:19 +0100

    refactors Header as functional component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,53 +1,46 @@
-import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './header.scss';
 import Logo from '../../assets/logo.png';
 
-export interface HeaderProps {
-  mainMenu: array;
-  topMenuId?: number;
-}
+const Header = ({mainMenu, topMenuId}) => {
+  const menuItems = mainMenu.map(function (name, index) {
+    const cssClass = 'nav-item';
 
-export class Header extends Component<HeaderProps> {
-  render() {
-    const { mainMenu, topMenuId } = this.props;
+    // apply active class if we are on the current
+    if (mainMenu[index] != null && mainMenu[index].id === topMenuId) {
+      cssClass += ' active';
+    }
+
     return (
-      <header>
-        <div className="container-fluid">
-          <div>
-            <img src={Logo} alt="Open Referral Logo" />
-          </div>
-        </div>
-        <div id="main_content_wrap" className="outer">
-          <section>
-            <div className="navbackground">
-              <nav className="navbar navbar-expand-md navwidth navbar-dark">
-                <ul className="navbar-nav mr-auto">
-                  {mainMenu.map(function (name, index) {
-                    var cssClass = 'nav-item';
-
-                    if (
-                      mainMenu[index] != null &&
-                      mainMenu[index].id === topMenuId
-                    ) {
-                      cssClass += ' active';
-                    }
-                    return (
-                      <li className={cssClass} key={mainMenu[index].link}>
-                        <Link className="nav-link" to={mainMenu[index].link}>
-                          {mainMenu[index].label}
-                        </Link>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </nav>
-            </div>
-          </section>
-        </div>
-      </header>
+      <li className={cssClass} key={mainMenu[index].link}>
+        <Link className="nav-link" to={mainMenu[index].link}>
+          {mainMenu[index].label}
+        </Link>
+      </li>
     );
-  }
+  })
+
+  return (
+    <header>
+      <div className="container-fluid">
+        <div>
+          <img src={Logo} alt="Open Referral Logo" />
+        </div>
+      </div>
+
+      <div id="main_content_wrap" className="outer">
+        <section>
+          <div className="navbackground">
+            <nav className="navbar navbar-expand-md navwidth navbar-dark">
+              <ul className="navbar-nav mr-auto">
+                {menuItems}
+              </ul>
+            </nav>
+          </div>
+        </section>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
