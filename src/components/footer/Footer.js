cddddfc4cf839853707d04a112dbbeafdf3926ed
yemijaiyeola <yemi.jaiyeola@wearesnook.com>
2021-04-07 10:30:48 +0100

    add footer files

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
 import Header from '../header/Header';
 
+
 class componentName extends Component {
   render() {
     return (
       <footer className="footer">
         <Header />
-        <div className="parent">   
-          <div>&copy; 2019-2021 Open Referral UK</div>
-          <div className="oruktools">
-            <ul>
+        <div className="footergrid">
+          <div className="foot-one">
+            <div className="about">General enquiries</div>
+            <div className="about">hello@openreferraluk.org</div>
+            </div>
+          <div className="foot-two">
+            <div className="about">Governance board</div>
+            </div>
+          <div className="foot-three">
+          <div className="about">About</div>
+          <div><ul><li>Home</li><li>About</li><li>How it works</li><li>Getting Started</li><li>Who we are</li><li>Contact us</li></ul></div></div>  {/*refactor make api call*/}
+          <div className="copy">&copy; 2019-2021 Open Referral UK</div>
+          <div> <ul>
               <li> Terms &amp; Conditions</li>
               <li>Pivacy Policy</li>
-            </ul>
-            </div>
-        
+            </ul></div>
         </div>
+        
       </footer>
     );
   }
