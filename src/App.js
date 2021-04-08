0fb5cc3ebb8b0fd1134029db8b8b54b891eebe41
PorismDominicSkinner <54143283+PorismDominicSkinner@users.noreply.github.com>
2021-04-08 15:23:45 +0100

    Added content page type and switch router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import { useState, useEffect } from 'react';
 import ReactMarkdown from 'react-markdown';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import '../node_modules/jquery/dist/jquery.min.js';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
-import SideMenu from './components/sidemenu/SideMenu';
+import Content from './components/content/Content';
 import './App.css';
 import { config } from './config.js';
 
 
 function App() {
 
+  const [apiData, setAPIData] = useState('');
   const [headerText, setHeaderText] = useState('');
   const [bodyText, setBodyText] = useState('');
   const [imageUrl, setImageUrl] = useState('');
-  const [subMenuId, setSubMenuId] = useState('');
   const [topMenuId, setTopMenuId] = useState('');
   const [mainMenu, setMainMenu] = useState([]);
-  const [subMenu, setSubMenu] = useState([]);
 
   useEffect(() => {
 	var pageUri = window.location.pathname;
@@ -29,12 +28,10 @@ function App() {
     fetch(config.STRAPI_BASE_URI + pageUri)
       .then((res) => res.json())
       .then((data) => {
+		setAPIData(JSON.stringify(data));
         // set data from strapi to state vars
         setHeaderText(data.title);
         setBodyText(data.projectOverview);
-        if (data.sub_menu) {
-          setSubMenuId(data.sub_menu.id);
-        }
         if (data.top_menu) {
           setTopMenuId(data.top_menu.id);
         }
@@ -48,41 +45,27 @@ function App() {
       .then((data) => setMainMenu(data));
   }, []);
 
-  useEffect(() => {
-    if (subMenuId) {
-      fetch(config.STRAPI_BASE_URI + config.SUB_MENU_URI_STUB + subMenuId)
-        .then((res) => res.json())
-        .then((data) => setSubMenu(data.MenuItem));
-    }
-  }, [subMenuId]);
-
-  let side;
-  let mainPanelCss = 'col-12';
-
-  if (subMenu && subMenu.length > 0) {
-    mainPanelCss = 'col-10';
-    side = <SideMenu subMenu={subMenu} />;
-  }
-
   return (
-    <Router>
+  <Router>
       <div className="App">
         <Header mainMenu={mainMenu} topMenuId={topMenuId} />
         <div className="container-fluid">
-          <div className="row">
-            {side}
-            <div className={mainPanelCss}>
-              <main>
-                <h2>{headerText}</h2>
-                <ReactMarkdown>{bodyText}</ReactMarkdown>
-                <img className="logo" src={imageUrl} alt="logo" />
-              </main>
-            </div>
-          </div>
+		<Switch>
+			<Route exact path="/">
+				<main>
+					<h1>{headerText}</h1>
+					<ReactMarkdown>{bodyText}</ReactMarkdown>
+					<img className="logo" src={imageUrl} alt="logo" />
+				</main>
+			</Route>
+			<Route>
+				<Content data={apiData}/>
+			</Route>
+		</Switch>
         </div>
         <Footer className="footer" />
       </div>
-    </Router>
+	</Router>
   );
 }
 
---
+++ src/components/content/Content.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import ReactHtmlParser from 'react-html-parser'; 
+
+export interface ContentProps {
+  data: object;
+}
+
+export class Content extends Component<ContentProps> {
+  render() {
+    const { data } = this.props;
+	if (!data){
+		return (<div></div>);
+	}
+	let apiData = JSON.parse(data);
+	if (!apiData){
+		return (<div></div>);
+	}
+	
+	const ReactDOM = require('react-dom')
+	const div = document.createElement('div');
+	div.innerHTML = "<h2>Header 1</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam felis neque, pretium nec auctor sed, pharetra in eros. Curabitur facilisis tellus ac tellus fringilla, eu varius est aliquet. Duis ac enim vel risus egestas rhoncus. In ligula nunc, tempus ut diam at, hendrerit pretium neque. Cras ornare lobortis suscipit. Duis quam mauris, venenatis id tortor non, tristique rutrum velit. Vivamus ac nisi malesuada, lobortis quam vel, aliquam ipsum. Fusce tincidunt ullamcorper mauris suscipit dictum. Maecenas blandit ultricies rutrum. Vestibulum ut massa id nulla euismod bibendum. Nam ac malesuada urna. Donec vulputate sapien non ullamcorper euismod. Fusce non pharetra dui. Phasellus sit amet commodo augue.</p><p>Fusce maximus faucibus est vel convallis. Vivamus ipsum leo, finibus a lacus a, vulputate ultricies felis. Vivamus sem ligula, fringilla eget convallis ut, lacinia ultricies massa. In sagittis ante a tincidunt posuere. Mauris fermentum posuere risus. Sed tortor dui, laoreet pulvinar ipsum vehicula, posuere hendrerit libero. Aliquam vitae justo et velit hendrerit imperdiet. Donec nunc lectus, rhoncus vitae nulla ac, lobortis ornare neque. Donec pellentesque enim est, at vulputate augue egestas at.</p><p>Praesent nisl nulla, hendrerit a elit non, mollis consectetur nulla. Suspendisse velit nunc, mattis nec mauris quis, posuere molestie lorem. In id tellus dapibus, dignissim justo et, pulvinar justo. In in mollis diam. Phasellus vel lacus ornare, facilisis libero sed, congue turpis. Cras eu pretium dui, eu malesuada nulla. Phasellus ac urna et est bibendum aliquet. Nullam rhoncus pretium nunc in lacinia.</p><h2>Header 2</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam felis neque, pretium nec auctor sed, pharetra in eros. Curabitur facilisis tellus ac tellus fringilla, eu varius est aliquet. Duis ac enim vel risus egestas rhoncus. In ligula nunc, tempus ut diam at, hendrerit pretium neque. Cras ornare lobortis suscipit. Duis quam mauris, venenatis id tortor non, tristique rutrum velit. Vivamus ac nisi malesuada, lobortis quam vel, aliquam ipsum. Fusce tincidunt ullamcorper mauris suscipit dictum. Maecenas blandit ultricies rutrum. Vestibulum ut massa id nulla euismod bibendum. Nam ac malesuada urna. Donec vulputate sapien non ullamcorper euismod. Fusce non pharetra dui. Phasellus sit amet commodo augue.</p><p>Fusce maximus faucibus est vel convallis. Vivamus ipsum leo, finibus a lacus a, vulputate ultricies felis. Vivamus sem ligula, fringilla eget convallis ut, lacinia ultricies massa. In sagittis ante a tincidunt posuere. Mauris fermentum posuere risus. Sed tortor dui, laoreet pulvinar ipsum vehicula, posuere hendrerit libero. Aliquam vitae justo et velit hendrerit imperdiet. Donec nunc lectus, rhoncus vitae nulla ac, lobortis ornare neque. Donec pellentesque enim est, at vulputate augue egestas at.</p><p>Praesent nisl nulla, hendrerit a elit non, mollis consectetur nulla. Suspendisse velit nunc, mattis nec mauris quis, posuere molestie lorem. In id tellus dapibus, dignissim justo et, pulvinar justo. In in mollis diam. Phasellus vel lacus ornare, facilisis libero sed, congue turpis. Cras eu pretium dui, eu malesuada nulla. Phasellus ac urna et est bibendum aliquet. Nullam rhoncus pretium nunc in lacinia.</p>"
+	var h2s = div.querySelectorAll('h2');
+	var sideMenu = '';
+	for (var i = 0; i < h2s.length; i++) {
+		var h2Text = h2s[i].innerHTML;
+		var h2Id = h2Text.replace(/\W/g, '-');
+		h2s[i].id = h2Id;
+		sideMenu += "<li class='nav-item'><a class='nav-link' href='#"+h2Id+"'>"+h2Text+"</a></li>";
+	}
+	
+    return (
+	 <div className="row">
+        <div className="col-2">
+		<div className="pl-3">In this section<hr className="mt-0 mb-0"></hr></div><ul id="ulSideMenu" className="nav flex-column">
+		{ReactHtmlParser(sideMenu)}
+		</ul>
+		</div>
+        <div className="col-10">
+			<main>
+				<h1>{apiData.Article.title}</h1>
+				{ReactHtmlParser(div.innerHTML)}
+			</main>
+		</div>
+	</div>
+    );
+  }
+}
+
+export default Content;
