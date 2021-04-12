e2ff249bcf1ee4ad27bc3990a244b37eba04de5c
PorismDominicSkinner <54143283+PorismDominicSkinner@users.noreply.github.com>
2021-04-12 09:14:37 +0100

    Fix to use config

--- src/helpers/ContentConsumer.js
+++ src/helpers/ContentConsumer.js
@@ -1,15 +1,12 @@
-//lets refactor this we can look at together but I have made this env var change because the url is likely to change
-const BASE_URL  = "http://54.78.155.180:1337";
-const LANDING_PAGE_URI = '/landing-page';
-const MENU_URI = '/top-menus';
+import { config } from '../config.js';
 
-export const fetchLandingPageContent = () => {
+export const fetchLandingPageContent = (pageUri) => {
   //consider using nlogger
-  return fetch(BASE_URL + LANDING_PAGE_URI)
+  return fetch(config.STRAPI_BASE_URI + pageUri)
     .then(res => res.json()).catch(err => { console.log(err); });
 }
 
 export const fetchMainMenuItems = () => {
-  return fetch(BASE_URL + MENU_URI)
+  return fetch(config.STRAPI_BASE_URI + config.MENU_URI_STUB)
     .then(res => res.json())
 }
