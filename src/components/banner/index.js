b20f55d68bed33bb7098b006c33071cb81b14b24
PorismDominicSkinner <54143283+PorismDominicSkinner@users.noreply.github.com>
2021-04-19 12:21:28 +0100

    Updated links

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -3,7 +3,7 @@ const Banner = () => {
 return (
 <div className="banner">
     <a href="#0">Openreferral UK</a>
-    <a href="#0" className="oruktools">
+    <a href="/documentation-and-tools" className="oruktools">
         Document &amp; Tools
     </a>
 </div>
