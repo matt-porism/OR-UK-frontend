2e2e572df5e8013529efad1f420062f6b39703d8
kingdomlevel <nllmrrs@aol.com>
2021-04-22 16:17:19 +0100

    updates developers link in footer

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -3,8 +3,8 @@ const Banner = () => {
 return (
 <div className="banner">
     <a href="#0">Openreferral UK</a>
-    <a href="/documentation-and-tools" className="oruktools">
-        Document &amp; Tools
+    <a href="/developers" className="oruktools">
+        For developers
     </a>
 </div>
     )
