9f4a21193ea8396ad24d653460dc59ee8a5be449
yemijaiyeola <yemi.jaiyeola@wearesnook.com>
2021-04-25 20:41:57 +0100

    add not-found-route

---
+++ src/components/errorpage/index.js
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+const NotFound = () => {
+    return (
+        <main className="main">
+        <p>Page not found</p>
+    </main>
+    )
+}
+export default NotFound;
