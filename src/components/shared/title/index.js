13dc97ccdb3bf22d5322a30db28caf238400f609
yemijaiyeola <yemi.jaiyeola@wearesnook.com>
2021-04-25 15:27:59 +0100

    footer-grid

--- src/components/shared/title/index.js
+++ src/components/shared/title/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
 
 
 const Title = ({title, id}) => {
 
     return <h2 key={id}>{title}</h2>
 
+}
+
+Title.propTypes = {
+    title: PropTypes.string.isRequired,
+    id: PropTypes.string
 }
 export default Title;
