801ac9ab615c8187021a7329de7baaed93c04e9f
Ananda <ananda.maryon@wearesnook.com>
2021-04-29 11:15:44 +0100

    home page hero banner styling

--- src/components/section/index.js
+++ src/components/section/index.js
@@ -1,17 +1,18 @@
 import ReactMarkdown from 'react-markdown';
 
 
-
-const Section = ({headingText, bodyText, styleName}) => {
+const Section = ({ headingText, bodyText }) => {
 
     return (
         <>
-        <section className={styleName}>
-        <h1>{headingText}</h1>
-        <ReactMarkdown>{bodyText}</ReactMarkdown>
-        </section>
+            <section className="hero-container">
+                <div>
+                    <h1>{headingText}</h1>
+                    <ReactMarkdown>{bodyText}</ReactMarkdown>
+                </div>
+            </section>
         </>
-   
-        )
-    }
-    export default Section;
+
+    )
+}
+export default Section;
