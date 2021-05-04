98fc8908aeac70188fb11b3dcbb719e7d0503a36
kingdomlevel <nllmrrs@aol.com>
2021-05-04 12:23:57 +0100

    refactors "numbers" component html structure on whos using page to match the one on home page

--- src/components/whoisusing/badge/index.js
+++ src/components/whoisusing/badge/index.js
@@ -2,30 +2,23 @@ import React from 'react';
 
 
 const Numbers = ( {OrganisationsIntroText, numbers, title, linkTitle }) => {
+
+    if (!numbers) return null;
     
     //return a single badge pass params
 
     const keyArray =  Object.keys(numbers).filter(n => n !== 'id' )
     
-    const numbersAray =  keyArray.map(x => {
+    const numbersAray =  keyArray.map(key => {
        
-        return <li className="usingcolumns" key={x}>{numbers[x]}</li>
-
-    } );
-
-    const keyAray =  keyArray.map(x => {
-       
-        return <li className="usingcolumns" key={x}>{x}</li>
+        return <li className="usingcolumns" key={key}><span>{numbers[key]}</span>{key}</li>
 
     } );
 
     return (
-        
-       numbers && 
-       <>
+        <div>
             <ul className="row listnostyle addlargefont">{numbersAray}</ul>
-            <ul className="row listnostyle">{keyAray}</ul>
-         </>
+        </div>
     );
 }
     export default Numbers;
