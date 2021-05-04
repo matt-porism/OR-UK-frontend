fb2f1e1f09246db1c01f41f0f964792b3d767e72
kingdomlevel <nllmrrs@aol.com>
2021-05-04 14:58:11 +0100

    removes unused vars in CompanyLogo component

--- src/components/artefacts/logo/index.js
+++ src/components/artefacts/logo/index.js
@@ -4,7 +4,7 @@ import  PropTypes  from 'prop-types';
 
 const CompanyLogo = ({logo}) => {
     const BASE_URL = process.env.REACT_APP_BASE_URL
-    const { formats, alternativeText, /*height, width,*/ id} = logo;
+    const { formats, alternativeText } = logo;
     //thumbnail does not work well for large screen spit out different image for large `formats.small ? ${BASE_URL}${formats.small.url}` : `${BASE_URL}${formats.thumbnail.url }`
 
     return (
