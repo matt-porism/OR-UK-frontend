c7b6eb0032d04edc03e00f2a1ef3ceb268d59bb2
kingdomlevel <nllmrrs@aol.com>
2021-05-04 13:02:27 +0100

    remove legacy props from Numbers component for whos using page

--- src/components/whoisusing/badge/index.js
+++ src/components/whoisusing/badge/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-const Numbers = ( {OrganisationsIntroText, numbers, title, linkTitle }) => {
+const Numbers = ({ numbers }) => {
 
     if (!numbers) return null;
     
