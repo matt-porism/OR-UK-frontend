58dca4d396ee8b995e41e14e84c2bfaf78b8995f
yemijaiyeola <yemi.jaiyeola@wearesnook.com>
2021-05-07 18:04:57 +0100

    add new video

--- src/components/shared/video/index.js
+++ src/components/shared/video/index.js
@@ -3,12 +3,12 @@
 const Video = ({ name, width, height, source }) => {
 
     return( 
-        //process.env.REACT_APP_VIDEO currently not able to set env on server
         <>
-        <iframe id="videobox" title="vimeo-player" src="https://player.vimeo.com/video/543850815" height={height} frameBorder="0" allowFullScreen></iframe>
-        {/* this may need adding to the backend */}
+        <iframe id="videobox" height={height} src="https://www.youtube.com/embed/dn1ryloOLvk"  title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        {/* this will ideally need adding to the backend to remove hard-coding*/}
         <div id="video-link"><a href="https://openreferraluk.org/open-referral-uk-video-transcript">View the full video transcript including visual description</a></div>
         </>
+       
     )
 }
 export default Video;
